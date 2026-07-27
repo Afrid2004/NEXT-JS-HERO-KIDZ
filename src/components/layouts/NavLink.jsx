73743dd2126a318b555395180c6686ba98f78276
afrid2004 <mdfaisalafrid@gmail.com>
2026-07-28 00:01:04 +0600

    added products

--- src/components/layouts/NavLink.jsx
+++ src/components/layouts/NavLink.jsx
@@ -6,10 +6,11 @@ import React from "react";
 const NavLink = ({ href, children }) => {
   // get the path
   const path = usePathname();
+  const isActive = href === "/" ? path === "/" : path.startsWith(href);
   return (
     <Link
       href={href}
-      className={`text-lg ${path.startsWith(href) ? "text-primary font-bold" : ""}`}
+      className={`text-lg ${isActive ? "text-primary font-bold" : ""}`}
     >
       {children}
     </Link>
