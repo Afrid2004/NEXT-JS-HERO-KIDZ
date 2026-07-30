5598d8fd8623aacdd920c6a614d10703b6fe0d5a
afrid2004 <mdfaisalafrid@gmail.com>
2026-07-30 19:03:01 +0600

    adde google sign in data into db

--- src/lib/authOptions.js
+++ src/lib/authOptions.js
@@ -1,6 +1,7 @@
 import { LoginUser } from "@/actions/server/Auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+import { collections, dbConnect } from "./dbConnect";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -21,4 +22,31 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+
+  callbacks: {
+    async signIn({ user, account }) {
+      const userCollection = await dbConnect(collections.USERS);
+
+      const isExistUser = await userCollection.findOne({
+        email: user.email,
+        provider: account.provider,
+      });
+
+      if (isExistUser) {
+        return true;
+      }
+
+      const newUser = {
+        provider: account.provider,
+        name: user.name,
+        email: user.email,
+        image: user.image,
+        role: "user",
+      };
+
+      const result = await userCollection.insertOne(newUser);
+
+      return result.acknowledged;
+    },
+  },
 };
